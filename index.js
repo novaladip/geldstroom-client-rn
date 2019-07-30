97ffa9433d62ca01b311a3821afeef6251c9146e
A. Noval Adip <a.novaladip@yahoo.com>
2019-07-30 07:31:10 +0700

    add asyncstorage

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { Navigation } from 'react-native-navigation';
+import AsyncStorage from '@react-native-community/async-storage';
 import { registerScreen } from './src/screen-config/registerScreen';
-import { AuthRoot, AppRoot } from './src/screen-config/configRoot';
+import { AuthRoot } from './src/screen-config/configRoot';
 
 registerScreen();
 
-Navigation.events().registerAppLaunchedListener(() => {
+Navigation.events().registerAppLaunchedListener(async () => {
   AuthRoot();
   // AppRoot();
 });
