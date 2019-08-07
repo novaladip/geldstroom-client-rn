d49e2a6a4489ff976f1ed7adda6276723eeeef97
A. Noval Adip <a.novaladip@yahoo.com>
2019-08-07 12:20:53 +0700

    redesigning Balance component

--- src/components/common/Balance/index.tsx
+++ src/components/common/Balance/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { styles } from './styles';
 import { formatIDR } from '../../../utils';
 import { BalanceState } from '../../../store/transaction/types';
@@ -12,20 +12,40 @@ export function Balance(props: Props) {
   const { income, expense } = props.balance;
   return (
     <View style={styles.container}>
-      <View style={[styles.balance, styles.total]}>
-        <Text style={styles.text}>Total: IDR{formatIDR(income - expense)}</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Today transactions overview</Text>
       </View>
-      <View style={styles.row}>
-        <View style={[styles.balance, styles.expense]}>
-          <Text style={[styles.text, styles.textExpense]}>
-            - IDR{formatIDR(expense)}
-          </Text>
-        </View>
-        <View style={[styles.balance, styles.income]}>
-          <Text style={[styles.text, styles.textIncome]}>
-            + IDR{formatIDR(income)}
-          </Text>
-        </View>
+      <View style={[styles.balance, styles.income, styles.borderBottom]}>
+        <Image
+          source={require('./assets/income.png')}
+          resizeMethod="auto"
+          resizeMode="center"
+          style={styles.icon}
+        />
+        <Text style={[styles.text, styles.textIncome]}>
+          IDR{formatIDR(income)}
+        </Text>
+      </View>
+
+      <View style={[styles.balance, styles.expense, styles.borderBottom]}>
+        <Image
+          source={require('./assets/expense.png')}
+          resizeMethod="auto"
+          resizeMode="center"
+          style={styles.icon}
+        />
+        <Text style={[styles.text, styles.textExpense]}>
+          IDR{formatIDR(expense)}
+        </Text>
+      </View>
+      <View style={[styles.balance, styles.total]}>
+        <Image
+          source={require('./assets/total.png')}
+          resizeMethod="auto"
+          resizeMode="center"
+          style={styles.icon}
+        />
+        <Text style={styles.text}>IDR{formatIDR(income - expense)}</Text>
       </View>
     </View>
   );
--- src/components/common/Balance/styles.ts
+++ src/components/common/Balance/styles.ts
@@ -3,38 +3,49 @@ import { colors } from '../../../utils';
 
 export const styles = StyleSheet.create({
   container: {
-    paddingVertical: 20,
-    paddingHorizontal: 10,
+    paddingBottom: 5,
     backgroundColor: colors.secondary,
   },
-  row: {
-    flexDirection: 'row',
+  titleContainer: {
+    backgroundColor: colors.secondaryDark,
     width: '100%',
-    justifyContent: 'center',
-    marginTop: 10,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: colors.secondary,
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 14,
+    color: colors.textOnPrimary,
+  },
+  icon: {
+    height: 32,
+    width: 32,
   },
   balance: {
     alignSelf: 'center',
-    width: '45%',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
     alignItems: 'center',
     padding: 10,
-    borderWidth: 1,
-    borderRadius: 10,
+  },
+  borderBottom: {
+    borderBottomColor: colors.textOnPrimary,
+    borderBottomWidth: 1,
   },
   income: {
-    borderColor: colors.income,
+    borderColor: colors.primary,
   },
   expense: {
-    borderColor: colors.expense,
-    marginRight: 10,
+    borderColor: colors.primaryDark,
   },
   total: {
-    borderColor: colors.secondaryLight,
+    borderColor: colors.primaryLight,
   },
   text: {
     fontSize: 15,
-    color: colors.textOnPrimary,
+    color: colors.textOnSecondary,
     fontWeight: 'bold',
   },
   textIncome: {
